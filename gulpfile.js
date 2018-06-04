ac42bd226904e6ac547374477f6c0f66dc577e50
Tristan VALCKE <valcketristan@gmail.com>
2018-06-04 23:19:41 +0200

    Gulpfile: Add doc task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,23 @@ const gulp     = require( 'gulp' )
 const jsdoc    = require( 'gulp-jsdoc3' )
 
 
+/**
+ * @method npm run doc
+ * @description Will generate this documentation
+ */
+gulp.task( 'doc', ( done ) => {
+
+    const config = require( './configs/jsdoc.conf' )
+    const files  = [
+        'README.md',
+        'gulpfile.js',
+        './configs/*.js',
+        './sources/**/*.js',
+        './tests/**/*.js'
+    ]
+
+    gulp.src( files, { read: false } )
+        .pipe( jsdoc( config, done ) )
+
+} )
+
