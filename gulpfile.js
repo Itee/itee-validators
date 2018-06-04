7c973635fe32b82d23e0c51291e77c4a6afb4d8c
Tristan VALCKE <valcketristan@gmail.com>
2018-06-04 23:20:21 +0200

    Gulpfile: Add description

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @module gulpfile
+ *
+ * @description The gulp tasks file. It allow to run some tasks from command line interface.<br>
+ * The available tasks are:
+ * <ul>
+ * <li>doc</li>
+ * </ul>
+ * You could find a complet explanation about these tasks using: <b>npm run help</b>.
+ *
+ * @requires {@link module: [gulp]{@link https://github.com/gulpjs/gulp}}
+ * @requires {@link module: [gulp-jsdoc3]{@link https://github.com/mlucool/gulp-jsdoc3}}
+ */
+
 
 const gulp     = require( 'gulp' )
 const jsdoc    = require( 'gulp-jsdoc3' )
