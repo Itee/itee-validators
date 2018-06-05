352f9e6049e1938e02ad2c7ec29b10795ca8e2da
Tristan VALCKE <valcketristan@gmail.com>
2018-06-05 11:02:36 +0200

    isFinite: Update doc

--- sources/cores/numbers/isFinite.js
+++ sources/cores/numbers/isFinite.js
@@ -1,17 +1,17 @@
 /**
  * @author [Tristan Valcke]{@link https://github.com/Itee}
- * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ * @license [MIT]{@link https://opensource.org/licenses/MIT}
  *
- * @file Todo
- *
- * @example Todo
+ * @module sources/cores/numbers/isFinite
+ * @desc Export the validation methods about numbers
  *
  */
 
 /**
+ * Check if the given data is a finite number
  *
- * @param data
- * @return {boolean}
+ * @param data {any} The data to check against the finite state
+ * @returns {boolean} true if data is finite, false otherwise
  */
 export function isFinite ( data ) {
     return Number.isFinite( data )
