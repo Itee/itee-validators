b13c68737c0efd701e62449c49763034c5b4fb5a
Tristan VALCKE <valcketristan@gmail.com>
2018-06-05 13:05:05 +0200

    Tests: Update licence documentation

--- tests/benchmarks/cores/booleans/booleans.benchs.js
+++ tests/benchmarks/cores/booleans/booleans.benchs.js
@@ -1,11 +1,7 @@
 
 /**
  * @author [Tristan Valcke]{@link https://github.com/Itee}
- * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
- *
- * @file Todo
- *
- * @example Todo
+ * @license [MIT]{@link https://opensource.org/licenses/MIT}
  *
  */
 
