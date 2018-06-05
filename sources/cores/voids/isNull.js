3d33f09f74e53ba90741d3e747fab66e9b9326f5
Tristan VALCKE <valcketristan@gmail.com>
2018-06-05 11:06:38 +0200

    isNull: Update doc

--- sources/cores/voids/isNull.js
+++ sources/cores/voids/isNull.js
@@ -1,17 +1,17 @@
 /**
  * @author [Tristan Valcke]{@link https://github.com/Itee}
- * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ * @license [MIT]{@link https://opensource.org/licenses/MIT}
  *
- * @file Todo
- *
- * @example Todo
+ * @module sources/cores/voids/isNull
+ * @desc Export the validation methods about nullity
  *
  */
 
 /**
  * Check if given data is null
- * @param data
- * @returns {boolean|*} true if data is null, false otherwise.
+ *
+ * @param data {any} The data to check against the nullity
+ * @returns {boolean} true if data is null, false otherwise.
  */
 export function isNull ( data ) {
     return (data === null)
@@ -19,8 +19,9 @@ export function isNull ( data ) {
 
 /**
  * Check if given data is not null
- * @param data
- * @returns {boolean|*} true if data is not null, false otherwise.
+ *
+ * @param data {any} The data to check against the nullity
+ * @returns {boolean} true if data is not null, false otherwise.
  */
 export function isNotNull ( data ) {
     return !isNull( data )
