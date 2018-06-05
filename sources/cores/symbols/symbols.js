4388424f711acfaa3508b3890049640bc6cc93dd
Tristan VALCKE <valcketristan@gmail.com>
2018-06-05 11:05:54 +0200

    Symbols: Update doc

--- sources/cores/symbols/symbols.js
+++ sources/cores/symbols/symbols.js
@@ -1,10 +1,10 @@
 /**
  * @author [Tristan Valcke]{@link https://github.com/Itee}
- * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ * @license [MIT]{@link https://opensource.org/licenses/MIT}
  *
- * @file Todo
- *
- * @example Todo
+ * @module sources/cores/symbols/symbols
+ * @desc This is the symbols export entry point.
+ * It expose all exports of the symbols validators files.
  *
  */
 
