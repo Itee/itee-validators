6e7d49c6d06e06e5d0a35d4d6e098d6b4ad39238
Tristan VALCKE <valcketristan@gmail.com>
2018-06-06 07:27:44 +0200

    Objects: Merge all files in one

--- sources/cores/objects.js
+++ sources/cores/objects.js
@@ -2,18 +2,38 @@
  * @author [Tristan Valcke]{@link https://github.com/Itee}
  * @license [MIT]{@link https://opensource.org/licenses/MIT}
  *
- * @module sources/cores/objects/isEmptyObject
- * @desc Export the validation methods about empty objects
+ * @module sources/cores/objects
+ * @desc Export the validation methods about objects
+ * @requires {@link module:sources/cores/voids/isNull}
  * @requires {@link module:sources/cores/voids/isEmpty}
  * @requires {@link module:sources/cores/objects/isObject}
- *
  */
 
-import { isObject } from './isObject'
+import { isNotNull } from './voids/isNull'
 import {
     isEmpty,
     isNotEmpty
-} from '../voids/isEmpty'
+} from './voids/isEmpty'
+
+/**
+ * Check if given data is an object
+ *
+ * @param data {any} The data to check against the object type
+ * @returns {boolean} true if data is object, false otherwise
+ */
+export function isObject ( data ) {
+    return ( isNotNull( data ) && (typeof data === 'object') && !Array.isArray( data ) )
+}
+
+/**
+ * Check if given data is not an object
+ *
+ * @param data {any} The data to check against the object type
+ * @returns {boolean} true if data is not an object, false otherwise
+ */
+export function isNotObject ( data ) {
+    return !isObject( data )
+}
 
 /**
  * Check if given data is an empty object
