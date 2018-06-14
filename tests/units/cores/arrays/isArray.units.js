48f0ea8a0a8b6b6e49f70fc1aeaad61deed9cda2
Tristan VALCKE <tristan.valcke@gmail.com>
2018-06-14 13:53:00 +0200

    isArray: Implementation

--- tests/units/cores/arrays/isArray.units.js
+++ tests/units/cores/arrays/isArray.units.js
@@ -16,9 +16,76 @@ function isArrayUnits () {
 
     describe( 'isArray()', () => {
 
-        it( 'should be implemented', () => {
+        it( 'should return false when the value is a void', () => {
 
-            expect( isArray( [] ) ).to.be.true
+            const dataSet = this._dataSet[ 'voids' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.false
+            }
+
+        } )
+
+
+        it( 'should return false when the value is a boolean', () => {
+
+            const dataSet = this._dataSet[ 'booleans' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.false
+            }
+
+        } )
+
+        it( 'should return false when the value is a number', () => {
+
+            const dataSet = this._dataSet[ 'numbers' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.false
+            }
+
+        } )
+
+        it( 'should return false when the value is a string', () => {
+
+            const dataSet = this._dataSet[ 'strings' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.false
+            }
+
+        } )
+
+        it( 'should return false when the value is a function', () => {
+
+            const dataSet = this._dataSet[ 'functions' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.false
+            }
+
+        } )
+
+        it( 'should return true when the value is an array', () => {
+
+            const dataSet = this._dataSet[ 'arrays' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.true
+            }
+
+        } )
+
+        it( 'should return false when the value is an typed array', () => {
+
+            const dataSet = this._dataSet[ 'typedArrays' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.false
+            }
+
+        } )
+
+        it( 'should return false when the value is an object', () => {
+
+            const dataSet = this._dataSet[ 'objects' ]
+            for ( let key in dataSet ) {
+                expect( isArray( dataSet[ key ] ) ).to.be.false
+            }
 
         } )
 
