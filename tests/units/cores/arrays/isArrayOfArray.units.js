1cb88672e4725c83e7a6ed608b262f4495d8cd8d
Tristan VALCKE <tristan.valcke@gmail.com>
2018-06-14 14:32:53 +0200

    isArrayOfArrayUnit: Style

--- tests/units/cores/arrays/isArrayOfArray.units.js
+++ tests/units/cores/arrays/isArrayOfArray.units.js
@@ -25,7 +25,6 @@ function isArrayOfArrayUnits () {
 
         } )
 
-
         it( 'should return false when the value is a boolean', () => {
 
             const dataSet = this._dataSet[ 'booleans' ]
