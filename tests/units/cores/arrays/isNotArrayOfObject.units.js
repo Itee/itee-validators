f6cab57bb9b5f11d3c4b35ed64f27832933d3b28
Tristan VALCKE <tristan.valcke@gmail.com>
2018-06-18 13:56:40 +0200

    isNotArrayOfObjectUnit: Fix strings test

--- tests/units/cores/arrays/isNotArrayOfObject.units.js
+++ tests/units/cores/arrays/isNotArrayOfObject.units.js
@@ -126,10 +126,10 @@ function isNotArrayOfObjectUnits () {
 
         } )
 
-        it( 'should return false when the value is an array of strings', () => {
+        it( 'should return true when the value is an array of strings', () => {
 
             const dataSet = this._dataSet[ 'arrays' ]
-            expect( isNotArrayOfObject( dataSet[ 'strings' ] ) ).to.be.false
+            expect( isNotArrayOfObject( dataSet[ 'strings' ] ) ).to.be.true
 
         } )
 
