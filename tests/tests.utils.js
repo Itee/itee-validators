cd8c1fb65334beef18e73ad7d4e3dc0b9769cb74
Tristan VALCKE <valcketristan@gmail.com>
2018-09-17 09:24:13 +0200

    TestsUtils: Implement createDataSet

--- tests/tests.utils.js
+++ tests/tests.utils.js
@@ -256,216 +256,47 @@ export function iterateOverDataMap ( func ) {
 
 }
 
-export function createDataSet ( dataNames ) {
+export function createDataSet ( dataSetOptions ) {
 
-    const voidDataSet = {
-        null:      null,
-        undefined: undefined,
-        void:      void(0)
-    }
+    if ( dataSetOptions === undefined ) {
 
-    const booleanDataSet = {
-        true:  true,
-        false: false
-    }
-
-    const numericDataSet = {
-        negativeInfinity:       Number.NEGATIVE_INFINITY,
-        negativeMaxValue:       -Number.MAX_VALUE,
-        negativeMinSafeInteger: Number.MIN_SAFE_INTEGER,
-        negativeMinValue:       -Number.MIN_VALUE,
-        negativeHexa:           -0x123456,
-        negativePow:            -2e+2,
-        negativeFloat:          -1.0,
-        negativeInt:            -1,
-        negativeNullDouble:     -0.0,
-        negativeNullInt:        -0,
-        nan:                    Number.NaN,
-        positiveNullInt:        0,
-        positiveNullFloat:      0.0,
-        positiveInt:            1,
-        positiveFloat:          1.0,
-        positivePow:            2e+2,
-        positiveHexa:           0x123456,
-        positiveMinValue:       Number.MIN_VALUE,
-        positiveMaxSafeInteger: Number.MAX_SAFE_INTEGER,
-        positiveMaxValue:       Number.MAX_VALUE,
-        positiveInfinity:       Number.POSITIVE_INFINITY
-    }
-
-    const stringDataSet = (() => {
-        'use strict'
-
-        const dataSet = {
-            empty:       '',
-            blank:       '      ',
-            stringNull:  new String(),
-            stringEmpty: new String( '' ),
-            stringBlank: new String( '    ' ),
-            foobar:      'foobar'
+        dataSetOptions = {
+            voids:       [],
+            booleans:    [],
+            numbers:     [],
+            strings:     [],
+            functions:   [],
+            arrays:      [],
+            typedArrays: [],
+            objects:     []
         }
 
-        for ( let i = 0, m = voidDataSet.length ; i < m ; i++ ) {
-            dataSet[ voidDataSet[ i ] ] = `${voidDataSet[ i ]}`
-        }
-
-        for ( let j = 0, n = booleanDataSet.length ; j < n ; j++ ) {
-            dataSet[ booleanDataSet[ j ] ] = `${booleanDataSet[ j ]}`
-        }
-
-        for ( let k = 0, o = numericDataSet.length ; k < o ; k++ ) {
-            dataSet[ numericDataSet[ k ] ] = `${numericDataSet[ k ]}`
-        }
-
-        return dataSet
-
-    })()
-
-    const functionDataSet = {
-        classicFunction: function emptyFct () {},
-        arrowFunction:   () => {}
     }
 
-    const arrayDataSet = (() => {
-        'use strict'
+    let dataSet = []
 
-        const dataSet = {
-            emptyArray:       [],
-            emptyArrayObject: new Array(),
-            singleValued:     [ 0 ],
-            multiValued:      [ 0, 1, 2 ],
-            null:             (() => {
+    for ( let optionKey in dataSetOptions ) {
 
-                const nullArray = []
+        const map    = globalDataMap[ optionKey ]
+        const option = dataSetOptions[ optionKey ]
 
-                for ( let index = 0 ; index < 3 ; index++ ) {
-                    nullArray.push( null )
-                }
+        if ( option.length === 0 ) {
 
-                return nullArray
+            for ( let valueKey in map ) {
+                dataSet.push( map[ valueKey ] )
+            }
 
-            })(),
-            undefined:        (() => {
+        } else {
 
-                const undefinedArray = []
+            for ( let i = 0, nbOptions = option.length ; i < nbOptions ; i++ ) {
+                dataSet.push( map[ option[ i ] ] )
+            }
 
-                for ( let index = 0 ; index < 3 ; index++ ) {
-                    undefinedArray.push( undefined )
-                }
-
-                return undefinedArray
-
-            })(),
-            void:             (() => {
-
-                const undefinedArray = []
-
-                for ( let index = 0 ; index < 3 ; index++ ) {
-                    undefinedArray.push( void(0) )
-                }
-
-                return undefinedArray
-
-            })(),
-            voids:            (() => {
-
-                const array = []
-
-                for ( let key in voidDataSet ) {
-                    array.push( voidDataSet[ key ] )
-                }
-
-                return array
-
-            })(),
-            booleans:         (() => {
-
-                const array = []
-
-                for ( let key in booleanDataSet ) {
-                    array.push( booleanDataSet[ key ] )
-                }
-
-                return array
-
-            })(),
-            numbers:          (() => {
-
-                const array = []
-
-                for ( let key in numericDataSet ) {
-                    array.push( numericDataSet[ key ] )
-                }
-
-                return array
-
-            })(),
-            strings:          (() => {
-
-                const array = []
-
-                for ( let key in stringDataSet ) {
-                    array.push( stringDataSet[ key ] )
-                }
-
-                return array
-
-            })(),
-            functions:        (() => {
-
-                const array = []
-
-                for ( let key in functionDataSet ) {
-                    array.push( functionDataSet[ key ] )
-                }
-
-                return array
-
-            })(),
-            objects:          [
-                {
-                    foo: 'bar'
-                },
-                {
-                    baz: 'qux'
-                }
-            ],
-            arrays:           [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]
         }
 
-        return dataSet
-
-    })()
-
-    const typedArrayDataSet = {
-        int8Array:    new Int8Array( [ 1, 2, 3 ] ),
-        uInt8Array:   new Uint8Array( [ 1, 2, 3 ] ),
-        int16Array:   new Int16Array( [ 1, 2, 3 ] ),
-        uInt16Array:  new Uint16Array( [ 1, 2, 3 ] ),
-        int32Array:   new Int32Array( [ 1, 2, 3 ] ),
-        uInt32Array:  new Uint32Array( [ 1, 2, 3 ] ),
-        float32Array: new Float32Array( [ 1.0, 2.0, 3.0 ] ),
-        float64Array: new Float64Array( [ 1.0, 2.0, 3.0 ] )
     }
 
-    const objectDataSet = [
-        {},
-        new Object(),
-        { null: null },
-        { undefined: undefined },
-        { foo: 'bar' }
-    ]
-
-    return {
-        voids:       voidDataSet,
-        booleans:    booleanDataSet,
-        numbers:     numericDataSet,
-        strings:     stringDataSet,
-        functions:   functionDataSet,
-        arrays:      arrayDataSet,
-        typedArrays: typedArrayDataSet,
-        objects:     objectDataSet
-    }
+    return dataSet
 
 }
 
@@ -473,8 +304,8 @@ export function createDataSetBenchmarkOptions ( datasetName ) {
 
     return {
 
-        setup:    function onSetup () {
-            this.dataset = Itee.TestsUtils.createDataMap()[datasetName]
+        setup: function onSetup () {
+            this.dataset = Itee.TestsUtils.createDataMap()[ datasetName ]
         },
 
         teardown: function onTeardown () {
