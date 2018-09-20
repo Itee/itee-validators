cda1e0a3904ce73b61e7411a618f1eb6decda8c4
Tristan VALCKE <valcketristan@gmail.com>
2018-09-20 11:18:30 +0200

    isNotEmptyObject: Fix wrong expected value

--- tests/units/cores/objects/isNotEmptyObject.units.js
+++ tests/units/cores/objects/isNotEmptyObject.units.js
@@ -16,11 +16,11 @@ function isNotEmptyObjectUnits () {
 
     describe( 'isNotEmptyObject()', () => {
 
-        it( 'should return false when the value is a void', () => {
+        it( 'should return true when the value is a void', () => {
 
             const values = this._dataMap.voids
             for( let key in values ) {
-                expect( isNotEmptyObject( values[ key ] ) ).to.be.false
+                expect( isNotEmptyObject( values[ key ] ) ).to.be.true
             }
 
         } )
