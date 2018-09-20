402f45381ea88fa25ce0f655cf4f2c341c867e68
Tristan VALCKE <valcketristan@gmail.com>
2018-09-21 00:31:29 +0200

    Fix missings commented lines

--- tests/benchmarks/cores/_cores.benchs.js
+++ tests/benchmarks/cores/_cores.benchs.js
@@ -5,11 +5,11 @@
  */
 
 export * from './arrays/_arrays.benchs'
-//export * from './booleans/_booleans.benchs'
-//export * from './functions/_functions.benchs'
-//export * from './numbers/_numbers.benchs'
-//export * from './objects/_objects.benchs'
-//export * from './strings/_strings.benchs'
-//export * from './symbols/_symbols.benchs'
-//export * from './voids/_voids.benchs'
+export * from './booleans/_booleans.benchs'
+export * from './functions/_functions.benchs'
+export * from './numbers/_numbers.benchs'
+export * from './objects/_objects.benchs'
+export * from './strings/_strings.benchs'
+export * from './symbols/_symbols.benchs'
+export * from './voids/_voids.benchs'
 
