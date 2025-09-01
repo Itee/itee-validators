30aff13c7713a24dc342ac08d2ea8bac25b6b0fb
Tristan VALCKE <valcketristan@gmail.com>
2025-09-02 01:42:56 +0200

    fix(configs): add missing rollup tests configs updates

--- configs/rollup.units.conf.js
+++ configs/rollup.units.conf.js
@@ -17,11 +17,10 @@ const cleanup         = require( 'rollup-plugin-cleanup' )
  * @generator
  * @return {Array.<json>} An array of rollup configuration
  */
-function CreateTestsRollupConfigs ( /*options*/ ) {
+function CreateUnitsRollupConfigs ( /*options*/ ) {
     'use strict'
 
     return [
-        /** Units **/
         // For karma
         {
             input:    `tests/units/${ packageInfos.name }.units.js`,
@@ -84,116 +83,9 @@ function CreateTestsRollupConfigs ( /*options*/ ) {
                 //                name:   'Itee.Units',
                 file:   `tests/builds/${ packageInfos.name }.units.cjs.js`
             }
-        },
-
-        /** Benchmarks **/
-        // For karma
-        {
-            input:    `tests/benchmarks/${ packageInfos.name }.benchs.js`,
-            external: [
-                'benchmark'
-            ],
-            plugins: [
-                nodeResolve(),
-                replace( {
-                    replaces: {
-                        '\tisBlockDevicePathSuite,':            '\t//isBlockDevicePathSuite,',
-                        '\tisCharacterDevicePathSuite,':        '\t//isCharacterDevicePathSuite,',
-                        '\tisDirectoryPathSuite,':              '\t//isDirectoryPathSuite,',
-                        '\tisEmptyDirectorySuite,':             '\t//isEmptyDirectorySuite,',
-                        '\tisEmptyFileSuite,':                  '\t//isEmptyFileSuite,',
-                        '\tisFIFOPathSuite,':                   '\t//isFIFOPathSuite,',
-                        '\tisFilePathSuite,':                   '\t//isFilePathSuite,',
-                        '\tisInvalidBlockDevicePathSuite,':     '\t//isInvalidBlockDevicePathSuite,',
-                        '\tisInvalidCharacterDevicePath,':      '\t//isInvalidCharacterDevicePath,',
-                        '\tisInvalidCharacterDevicePathSuite,': '\t//isInvalidCharacterDevicePathSuite,',
-                        '\tisInvalidDirectoryPathSuite,':       '\t//isInvalidDirectoryPathSuite,',
-                        '\tisInvalidFIFOPathSuite,':            '\t//isInvalidFIFOPathSuite,',
-                        '\tisInvalidFilePathSuite,':            '\t//isInvalidFilePathSuite,',
-                        '\tisInvalidPathSuite,':                '\t//isInvalidPathSuite,',
-                        '\tisInvalidSocketPathSuite,':          '\t//isInvalidSocketPathSuite,',
-                        '\tisInvalidSymbolicLinkPathSuite,':    '\t//isInvalidSymbolicLinkPathSuite,',
-                        '\tisNotBlockDevicePathSuite,':         '\t//isNotBlockDevicePathSuite,',
-                        '\tisNotCharacterDevicePathSuite,':     '\t//isNotCharacterDevicePathSuite,',
-                        '\tisNotDirectoryPathSuite,':           '\t//isNotDirectoryPathSuite,',
-                        '\tisNotEmptyDirectorySuite,':          '\t//isNotEmptyDirectorySuite,',
-                        '\tisNotEmptyFileSuite,':               '\t//isNotEmptyFileSuite,',
-                        '\tisNotFIFOPathSuite,':                '\t//isNotFIFOPathSuite,',
-                        '\tisNotFilePathSuite,':                '\t//isNotFilePathSuite,',
-                        '\tisNotSocketPathSuite,':              '\t//isNotSocketPathSuite,',
-                        '\tisNotSymbolicLinkPathSuite,':        '\t//isNotSymbolicLinkPathSuite,',
-                        '\tisSocketPathSuite,':                 '\t//isSocketPathSuite,',
-                        '\tisSymbolicLinkPathSuite,':           '\t//isSymbolicLinkPathSuite,',
-                        '\tisValidBlockDevicePathSuite,':       '\t//isValidBlockDevicePathSuite,',
-                        '\tisValidCharacterDevicePathSuite,':   '\t//isValidCharacterDevicePathSuite,',
-                        '\tisValidDirectoryPathSuite,':         '\t//isValidDirectoryPathSuite,',
-                        '\tisValidFIFOPathSuite,':              '\t//isValidFIFOPathSuite,',
-                        '\tisValidFilePathSuite,':              '\t//isValidFilePathSuite,',
-                        '\tisValidPathSuite,':                  '\t//isValidPathSuite,',
-                        '\tisValidSocketPathSuite,':            '\t//isValidSocketPathSuite,',
-                        '\tisValidSymbolicLinkPathSuite,':      '\t//isValidSymbolicLinkPathSuite,',
-
-                        'suite.run()': '//suite.run()'
-                    }
-                } ),
-                cleanup( {
-                    comments: 'none'
-                } )
-            ],
-            treeshake: {
-                moduleSideEffects:                false,
-                annotations:                      true,
-                correctVarValueBeforeDeclaration: true,
-                propertyReadSideEffects:          false,
-                tryCatchDeoptimization:           true,
-                unknownGlobalSideEffects:         false
-            },
-            output: {
-                indent:  '\t',
-                format:  'iife',
-                name:    'Itee.Benchmarks',
-                globals: {
-                    'benchmark': 'Benchmark'
-                },
-                file: `tests/builds/${ packageInfos.name }.benchs.iife.js`
-            }
-        },
-        // For Node
-        {
-            input:    `tests/benchmarks/${ packageInfos.name }.benchs.js`,
-            external: [
-                'benchmark',
-                'itee-utils',
-                'fs'
-            ],
-            plugins: [
-                nodeResolve(),
-                cleanup( {
-                    comments: 'none'
-                } )
-            ],
-            treeshake: false,
-            output:    {
-                indent: '\t',
-                format: 'cjs',
-                file:   `tests/builds/${ packageInfos.name }.benchs.cjs.js`
-            }
         }
-        // Utils for karma
-        //        {
-        //            input:     `tests/utils/${ packageInfos.name }.utils.js`,
-        //            plugins:   [],
-        //            treeshake: true,
-        //            output:    {
-        //                indent: '\t',
-        //                format: 'iife',
-        //                name:   'Itee',
-        //                file:   `tests/builds/${ packageInfos.name }.utils.iife.js`,
-        //                extend: true
-        //            }
-        //        }
     ]
 
 }
 
-module.exports = CreateTestsRollupConfigs
+module.exports = CreateUnitsRollupConfigs
