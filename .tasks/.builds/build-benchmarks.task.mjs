ec28a130e1cb08f1810b34649f7cb2691e70a3eb
Tristan VALCKE <valcketristan@gmail.com>
2025-11-04 19:37:57 +0100

    :art: (tasks) fix some task names, and split builds into their own config

--- .tasks/.builds/build-benchmarks.task.mjs
+++ .tasks/.builds/build-benchmarks.task.mjs
@@ -1,13 +1,13 @@
-import { series }                from 'gulp'
-import { bundleBenchmarksTask }  from '../.tests/benchmarks/bundle-benchmarks.task.mjs'
-import { computeBenchmarksTask } from '../.tests/benchmarks/compute-benchmarks.task.mjs'
+import { series }                     from 'gulp'
+import { buildBenchmarksBackendTask } from '../.tests/benchmarks/build-benchmarks-backend.task.mjs'
+import { computeBenchmarksTask }      from '../.tests/benchmarks/compute-benchmarks.task.mjs'
 
 /**
  * @description Will compute and generate bundle for benchmarks
  */
 const buildBenchmarksTask       = series(
     computeBenchmarksTask,
-    bundleBenchmarksTask,
+    buildBenchmarksBackendTask,
 )
 buildBenchmarksTask.displayName = 'build-benchmarks'
 buildBenchmarksTask.description = 'Will compute and generate bundle for benchmarks.'
--- .tasks/.builds/build-unit-tests.task.mjs
+++ .tasks/.builds/build-unit-tests.task.mjs
@@ -1,13 +1,13 @@
-import { series }               from 'gulp'
-import { bundleUnitTestsTask }  from '../.tests/unit-tests/bundle-unit-tests.task.mjs'
-import { computeUnitTestsTask } from '../.tests/unit-tests/compute-unit-tests.task.mjs'
+import { series }                    from 'gulp'
+import { buildUnitTestsBackendTask } from '../.tests/unit-tests/build-unit-tests-backend.task.mjs'
+import { computeUnitTestsTask }      from '../.tests/unit-tests/compute-unit-tests.task.mjs'
 
 /**
  * @description Will compute and generate bundle for unit tests
  */
 const buildUnitTestsTask       = series(
     computeUnitTestsTask,
-    bundleUnitTestsTask,
+    buildUnitTestsBackendTask,
 )
 buildUnitTestsTask.displayName = 'build-unit-tests'
 buildUnitTestsTask.description = 'Will compute and generate bundle for unit tests.'
