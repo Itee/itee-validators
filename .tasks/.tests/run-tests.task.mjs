876ba94473c63f8c11935240f94c52e6e81311ea
Tristan VALCKE <valcketristan@gmail.com>
2025-11-04 22:08:33 +0100

    :bug: (run-tests) fix task displayName

--- .tasks/.tests/run-tests.task.mjs
+++ .tasks/.tests/run-tests.task.mjs
@@ -11,7 +11,7 @@ const runTestsTask       = series(
     runBenchmarksTestsTask,
     runUnitTestsTask,
 )
-runTestsTask.displayName = 'run-unit-tests'
+runTestsTask.displayName = 'run-tests'
 runTestsTask.description = 'Will run unit tests and benchmarks for backend (node) and frontend (web-test-runner) environments.'
 runTestsTask.flags       = null
 
